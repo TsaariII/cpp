aa821ca0309201458a63bb10f9feed746e732116
¨Nikolai <nzharkev@student.hive.fi>
2025-03-14 16:33:16 +0200

    Saving

--- module_01/ex04/sources/main.cpp
+++ module_01/ex04/sources/main.cpp
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/14 11:34:08 by nzharkev          #+#    #+#             */
-/*   Updated: 2025/03/14 14:03:36 by nzharkev         ###   ########.fr       */
+/*   Created: 2025/03/14 14:06:37 by nzharkev          #+#    #+#             */
+/*   Updated: 2025/03/14 16:33:02 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/HumanB.hpp"
 #include <iostream>
+#include <fstream>
 
-HumanB::HumanB(std::string name) : _Name(name) {};
-HumanB::~HumanB() {};
-void HumanB::setWeapon(Weapon &weapon) { _Weapon = weapon; };
-void HumanB::attack() { std::cout << _Name << " attacks with their " << _Weapon.getType() << std::endl; };
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cerr << "To use this enter " << argv[0] << " <filename> <s1> <s2>" << std::endl;
+	}
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[2];
+	if (s1.empty())
+	{
+		std::cerr << "The s1 cannot be empty" << std::endl;
+	}
+}
